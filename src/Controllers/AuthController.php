d6d134093d3a7401c65397aed113e9195bafa6cf
yassinmftah <yassinmaftah06@gmail.com>
2026-01-15 15:45:23 +0100

    AuthController with 6 methds login signup logout and methods that display Views

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -16,7 +16,7 @@ class AuthController
         $password = $_POST['password'];
         $userModel = new User();
         if ($userModel->register($username, $email, $password)) {
-            header('Location: /Loyalty-Points-System/public/login');
+            header('Location: /Loyalty-Points-System/login');
             exit;
         } else
             echo "Error: Could not register user.";
@@ -31,14 +31,14 @@ class AuthController
         if ($user) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_name'] = $user['name'];
-            header('Location: /Loyalty-Points-System/public/home');
+            header('Location: /Loyalty-Points-System/home');
             exit;
         } else
             echo "Wrong email or password!";
     }
     public function logout() {
         session_destroy();
-        header('Location: /Loyalty-Points-System/public/login');
+        header('Location: /Loyalty-Points-System/login');
         exit;
     }
 }
