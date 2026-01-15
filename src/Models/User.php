a6799da6f2f84f30c557d1feb7b63a22f9f2d601
yassinmftah <yassinmaftah06@gmail.com>
2026-01-15 13:03:58 +0100

    Class user with two methods login ans signup

---
+++ src/Models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+use App\Core\Database;
+use PDO;
+
+
+class User
+{
+    private $db;
+    public function __construct() 
+    {
+        $obj_db = new Database();
+        $database = $obj_db->connect();
+        $this->db = $database;
+    }
+    public function login($email, $password)
+    {
+        $database = $this->db;
+        $sql = "SELECT * FROM users WHERE email = ?";
+        $stmt = $database->prepare($sql);
+        $stmt->execute([$email]);
+        $user = $stmt->fetch();
+        if ($user && password_verify($password, $user['password_hash']))
+            return $user;
+        return 0;
+    }
+    public function register($username, $email, $password)
+    {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (email, password_hash, name) VALUES (?,?,?)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$email,$hashedPassword, $username]);
+    }
+}
