c73f058d17ff621922d2327817dac7bfe263e751
yassinmftah <yassinmaftah06@gmail.com>
2026-01-15 15:41:39 +0100

    change All path in form action

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,8 +1,8 @@
 <?php if (isset($_SESSION['user_name'])): ?>
     <h1>Hello <?= $_SESSION['user_name']; ?></h1>
     <p>You are logged in.</p>
-    <a href="/Loyalty-Points-System/public/logout">Logout</a>
+    <a href="/Loyalty-Points-System/logout">Logout</a>
 <?php else: ?>
     <h1>Welcome Visitor</h1>
-    <a href="/Loyalty-Points-System/public/login">Login</a>
+    <a href="/Loyalty-Points-System/login">Login</a>
 <?php endif; ?>
--- src/Views/signup.php
+++ src/Views/signup.php
@@ -1,8 +1,8 @@
 <h1>Signup</h1>
-<form action="/Loyalty-Points-System/public/signup" method="POST">
+<form action="/Loyalty-Points-System/signup" method="POST">
     <input type="text" name="username" placeholder="Username" required><br>
     <input type="email" name="email" placeholder="Email" required><br>
     <input type="password" name="password" placeholder="Password" required><br>
     <button type="submit">Create Account</button>
 </form>
-<a href="/Loyalty-Points-System/public/login">Already have an account? Login</a>
+<a href="/Loyalty-Points-System/login">Already have an account? Login</a>
