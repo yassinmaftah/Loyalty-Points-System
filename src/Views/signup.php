f44955814fbe8a872c02ee8fe2aabb738b53c237
yassinmftah <yassinmaftah06@gmail.com>
2026-01-15 12:53:59 +0100

    form signup Using PHP HTML just for test

---
+++ src/Views/signup.php
@@ -0,0 +1,8 @@
+<h1>Signup</h1>
+<form action="/Loyalty-Points-System/public/signup" method="POST">
+    <input type="text" name="username" placeholder="Username" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Create Account</button>
+</form>
+<a href="/Loyalty-Points-System/public/login">Already have an account? Login</a>
