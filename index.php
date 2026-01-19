cb67dcf944c12ca2d658c8b0ab4e080a92ce5c5d
yassinmftah <yassinmaftah06@gmail.com>
2026-01-19 11:22:42 +0100

    add 7 new routers and configuration twig

--- index.php
+++ index.php
@@ -3,17 +3,32 @@ session_start();
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
 use App\Core\Router;
 $router = new Router();
 
-$router->add('GET', '/home', 'AuthController', 'home');
+$router->add('GET', '/', 'AuthController', 'home');
 $router->add('GET', '/login', 'AuthController', 'showLoginForm');
 $router->add('POST', '/login', 'AuthController', 'login');
 $router->add('GET', '/signup', 'AuthController', 'showSignupForm');
 $router->add('POST', '/signup', 'AuthController', 'signup');
 $router->add('GET', '/logout', 'AuthController', 'logout');
+
+$router->add('GET', '/shop', 'ShopController', 'ShoP_Page');
+$router->add('POST', '/shop/add', 'ShopController', 'add');
+$router->add('GET', '/cart', 'ShopController', 'viewCart');
+$router->add('GET', '/checkout', 'ShopController', 'checkout');
+$router->add('GET', '/rewards', 'ShopController', 'showRewards');
+$router->add('POST', '/shop/redeem', 'ShopController', 'redeemPoints');
+$router->add('GET', '/history', 'ShopController', 'history');
+
 $requestUri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-$scriptName = dirname($_SERVER['SCRIPT_NAME']);
-$requestUri = str_replace($scriptName, '', $requestUri);
-$router->dispatch($requestUri, $requestMethod);
+
+$requestUri = str_replace(dirname($_SERVER['SCRIPT_NAME']), '', $requestUri);
+
+$router->dispatch($requestUri, $requestMethod, $twig);
