7326fd56d0fa2975f70a5f2cfffc27f6afb79693
yassinmftah <yassinmaftah06@gmail.com>
2026-01-21 20:09:14 +0100

    deleted

--- src/Views/login.php
+++
@@ -1,7 +0,0 @@
-<h1>Login</h1>
-<form action="/Loyalty-Points-System/login" method="POST">
-    <input type="email" name="email" placeholder="Email" required><br>
-    <input type="password" name="password" placeholder="Password" required><br>
-    <button type="submit">Login</button>
-</form>
-<a href="/Loyalty-Points-System/signup">Create an account</a>
