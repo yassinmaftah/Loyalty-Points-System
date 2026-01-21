eecd5fae6544df0650602b3b31ab12dea1751954
yassinmftah <yassinmaftah06@gmail.com>
2026-01-21 16:39:50 +0100

    feat(core): implement database connection class using PDO

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -5,25 +5,24 @@ namespace App\Core;
 use PDO;
 use PDOException;
 
-class   Database
+class Database
 {
     private $host = "localhost";
-    private $dbname = "LoyaltyPointsSystem";
-    private $user = "root";
-    private $pass = "sqlyassine2025";
+    private $db_name = "loyaltypointssystem";
+    private $username = "root";
+    private $password = "sqlyassine2025";
+    private $conn;
 
     public function connect()
     {
-        try
-        {
-            $pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname;", $this->user, $this->pass);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            return $pdo;
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name,$this->username,$this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo " Data base Not Connected" . $exception->getMessage();
         }
-        catch (PDOException $e)
-        {
-            die("Connection failed: " . $e->getMessage());
-        }
+        return $this->conn;
     }
 }
